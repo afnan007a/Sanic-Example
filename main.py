6cf936e184ea1370e5a1b32b4c58cd29872120b6
Giyu <72243953+afnan007a@users.noreply.github.com>
2021-08-14 13:30:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+from sanic import Sanic
+from sanic.response import json, html, text
+import os
+
+app = Sanic(__name__)
+
+@app.route('/')
+def index(request):
+  template = open(os.getcwd() + "/templates/index.html")
+  return html(template.read())
+
+app.run(host='0.0.0.0', port=4044)
